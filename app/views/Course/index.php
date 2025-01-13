ad759752002fce716672a420250ff34d7fc006d0
Iftikar Hussain <iftiy.muzik@gmail.com>
2025-01-13 19:03:12 +0530

    Faculty Version Single 3

--- app/views/Course/index.php
+++ app/views/Course/index.php
@@ -71,7 +71,7 @@
                                 </div>
                             </div><!-- /.row -->
                         </div><!-- /.container-fluid -->
-                                  
+                    
                     </div>
 
                 </div>
